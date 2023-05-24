12aa309993873ffe438746061ab93a984555e0b9
deepdreamapps <deepdreamapps@gmail.com>
2023-05-24 18:42:55 -0400

    Exclude JPA automatic configuration

--- src/main/java/tech/deepdreams/messaging/ManagementMessagingApplication.java
+++ src/main/java/tech/deepdreams/messaging/ManagementMessagingApplication.java
@@ -2,13 +2,15 @@ package tech.deepdreams.messaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EntityScan(basePackages = {"tech.deepdreams.messaging.models"})
 @EnableJpaRepositories("tech.deepdreams.messaging.repositories")
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ManagementMessagingApplication {
 
 	public static void main(String[] args) {
