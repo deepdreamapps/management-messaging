c97bb967805f15e4a2b305a58bd04cfdc95b6482
deepdreamapps <deepdreamapps@gmail.com>
2023-05-28 01:48:54 -0400

    Update project

--- src/main/java/tech/deepdreams/messaging/endpoints/SubscriptionEventEndpoint.java
+++ src/main/java/tech/deepdreams/messaging/endpoints/SubscriptionEventEndpoint.java
@@ -1,4 +1,5 @@
 package tech.deepdreams.messaging.endpoints;
+
 import java.time.OffsetDateTime;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -6,22 +7,19 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import tech.deepdreams.messaging.services.SubscriptionService;
 
-
 @Log4j2
 @AllArgsConstructor
 @Service
-public class SubscriptionEventEndpoint { 
-	private SubscriptionService  subscriptionService ;
-	
-	
-	
-	@Scheduled(fixedDelay = 30_000)
-	public void handleCreatedEvent () {
-		log.info(String.format("SubscriptionEventEndpoint.handleCreatedEvent : Execution time %s", OffsetDateTime.now())) ;
-		subscriptionService.fetchFromCreatedQueue()
-		           .forEach(message -> {
-		        	   log.info(String.format("Message received %s", message)) ;
+public class SubscriptionEventEndpoint {
+	private SubscriptionService subscriptionService;
 
-		           }) ;
+	@Scheduled(fixedDelay = 30_000)
+	public void handleCreatedEvent() {
+		log.info(String.format("SubscriptionEventEndpoint.handleCreatedEvent : Execution time %s",
+				OffsetDateTime.now()));
+		subscriptionService.fetchFromCreatedQueue().forEach(message -> {
+			log.info(String.format("Message received %s", message));
+
+		});
 	}
 }
